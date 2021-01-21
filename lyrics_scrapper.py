5400112f419d01d876f3b43303ae302639bedd3d
oscar.kirschstein <oscar.kirschstein@estudiante.uam.es>
2021-01-21 16:56:34 +0100

    Descargadas canciones de artistas.

--- lyrics_scrapper.py
+++ lyrics_scrapper.py
@@ -7,14 +7,12 @@ modelo generador.
 
   scrape_lyrics(path)
 """
-import urllib
-
-import urllib3
 import requests
 from bs4 import BeautifulSoup
 from pandas import DataFrame, read_csv, read_pickle
 import numpy as np
-from os.path import isfile
+from os.path import isfile, exists, isdir
+import os
 
 _URL_API = "https://api.genius.com/" # GENIUS API URL
 _URL_ARTIST = "artists/"
@@ -23,11 +21,15 @@ _TOKEN_GENIUS_API = "CATTyVT506krN0vbogZTybp72Ciqg7fFw8Ua9DnXeIsarXB8TX2hGY-EKtZ
 _NOMBRE_COL_IDS = "ID_CANCION"
 _NOMBRE_COL_LYR_URL = "URL_LYRICAS"
 _NOMBRE_COL_TIT = "TITULO_CANCION"
+_NOMBRE_COL_ART = "ARTISTA"
 _NOMBRE_COL_LETRA = "LETRA"
 _DF_CANCIONES = "canciones.pkl"
-_URI_DF_CANCIONES = "./data/" + _DF_CANCIONES
+_DIR_DATA = "./data/"
+_DIR_LETRAS = "letras/"
+_URI_DF_CANCIONES = _DIR_DATA + _DF_CANCIONES
+_URI_LETRAS = _DIR_DATA + _DIR_LETRAS
 _DF_LETRAS = "letras.pkl"
-_URI_DF_LETRAS = "./data/"+ _DF_LETRAS
+_URI_DF_LETRAS = _DIR_DATA + _DF_LETRAS
 
 """Descarga metadatos de canciones de https://genius.com/.
 
@@ -67,7 +69,7 @@ def get_metadatos_canciones(artists_path="./data/Artistas_IDs.csv"):
 	ids_canciones = [] # Lista de ids de las canciones
 	urls_lyr_can = [] # Lista de URLs de las líricas de las canciones
 	titulos = [] # Lista de los titulos de las canciones
-
+	artistas = [] # Lista de los artistas de las canciones
 	for _, fila in art_df.iterrows():
 		artista = fila["ARTISTA"]
 		id = fila["ID"]
@@ -96,47 +98,76 @@ def get_metadatos_canciones(artists_path="./data/Artistas_IDs.csv"):
 				urls_lyr_can.append(cancion["url"])
 
 				titulo = cancion["full_title"]
+				artistas.append(artista)
 				titulos.append(titulo)
 				num_canciones += 1
 		print(f"-------------------------------------------------------\nDescargada la metainformación de las canciones de \"{artista}\" ({num_canciones}).")
 
-	can_df = DataFrame(data=np.array([ids_canciones,urls_lyr_can, titulos]).T, columns=[_NOMBRE_COL_IDS, _NOMBRE_COL_LYR_URL, _NOMBRE_COL_TIT])
+	can_df = DataFrame(data=np.array([ids_canciones,urls_lyr_can, titulos, artistas]).T, columns=[_NOMBRE_COL_IDS, _NOMBRE_COL_LYR_URL, _NOMBRE_COL_TIT, _NOMBRE_COL_ART])
 	can_df = can_df.groupby([_NOMBRE_COL_IDS], as_index=False).first() # Conseguimos las canciones únicas (por si hay repetidas)
 
 	can_df.to_pickle(_URI_DF_CANCIONES)
 
 def get_liricas():
 
+	if not exists(_URI_LETRAS):
+		print("Creando carpeta con canciones")
+		os.mkdir(_URI_LETRAS)
+
 	letras_df = read_pickle(_URI_DF_CANCIONES)
-
-	letras_df[_NOMBRE_COL_LETRA] = None
-
-	letras_error = 0
-	for _, row in letras_df.iterrows():
-		try:
-
-			page = requests.get(row[_NOMBRE_COL_LYR_URL])
-		except requests.exceptions.RequestException:
-			raise requests.exceptions.RequestException(f"Ha ocurrido un error al descargar la lírica de la canción \"{row[_NOMBRE_COL_TIT]}\"")
-
-		html = BeautifulSoup(page.text, "html.parser")  # Extract the page's HTML as a string
-		# Scrape the song lyrics from the HTML
+	letras_df.sort_values(by=_NOMBRE_COL_ART)
 
 
-		lyrics = html.find("div", class_="lyrics")
+	for artista in letras_df[_NOMBRE_COL_ART].unique():
 
-		if lyrics is not None:
-			lyrics = lyrics.get_text()
-		else:
-			letras_error+=1
-
-	total_canciones = letras_df.shape[0]
-	letras_desc = total_canciones - letras_error
-
-	print(f"-------------------------------------------------------\nSe han podido descargas las líricas de {letras_desc}/{total_canciones} canciones...")
+		print(f"Se va a proceder a descargar las letras de {artista}")
+		letras_error = 0
 
 
-	letras_df.to_pickle(_URI_DF_LETRAS)
+		_URI_LETRAS_ARTISTA = _URI_LETRAS + artista + ".pkl"
+
+		if exists(_URI_LETRAS_ARTISTA): # Si ya existe el archivo de ese artista, se pasa a la siguiente iteración
+			print(f"Ya fueron descargadas las líricas de las canciones de \"{artista}\". Saltando...")
+			continue
+
+		canart_df = letras_df[letras_df[_NOMBRE_COL_ART] == artista]
+		total_canciones = canart_df.shape[0]
+
+		letras = [] # Lista que contendrá las letras de un artista
+		ids_can = [] # Lista que contendrá los ids de las canciones
+
+		n_can = 0
+		print(f"{n_can}/{total_canciones} de {artista} descargadas...")
+		for _, row in canart_df.iterrows():
+
+			try:
+				page = requests.get(row[_NOMBRE_COL_LYR_URL])
+			except requests.exceptions.RequestException:
+				raise requests.exceptions.RequestException(f"Ha ocurrido un error al descargar la lírica de la canción \"{row[_NOMBRE_COL_TIT]}\"...")
+
+			html = BeautifulSoup(page.text, "html.parser")  # Extract the page's HTML as a string
+
+			# Scrape the song lyrics from the HTML
+			lyrics = html.find("div", class_="lyrics")
+
+			if lyrics is not None:
+				letras.append(lyrics.get_text())
+				ids_can.append(row[_NOMBRE_COL_IDS])
+				n_can += 1
+				print(f"{n_can}/{total_canciones} de {artista} descargadas...")
+			else:
+				letras_error+=1
+
+		# Se guarda el df con todas las letras descargadas
+		DataFrame(data=np.array([ids_can, letras]).T,
+		          columns=[_NOMBRE_COL_IDS, _NOMBRE_COL_LETRA]).to_pickle(_URI_LETRAS_ARTISTA)
+
+		letras_desc = total_canciones - letras_error
+
+		print(f"-------------------------------------------------------")
+
+
+
 
 if __name__ == '__main__':
 	get_metadatos_canciones()
